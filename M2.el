4edf6676268c2acd6f4a4e51a205db068ac757e3
Doug Torrance <dtorrance@piedmont.edu>
2023-02-06 20:49:56 -0500

    Make colon at end of file position regular expression optional

--- M2.el
+++ M2.el
@@ -369,7 +369,7 @@ can be executed with \\[M2-send-to-program]."
 	   ;; * a pipe, for paths enclosed in a box, e.g.,:
 	   ;;   | ../../m2/res.m2:210:45-214:6:
 	   ;;                (1                     1)   (2       2)   (3      3)   (4      4)   (5      5)   (6      6))
-	   (looking-at "^ *\\(o+[1-9][0-9]* = \\|| \\)?\\([^:\n]+\\):\\([0-9]+\\):\\([0-9]+\\)-\\([0-9]+\\):\\([0-9]+\\):"))
+	   (looking-at "^ *\\(o+[1-9][0-9]* = \\|| \\)?\\([^:\n]+\\):\\([0-9]+\\):\\([0-9]+\\)-\\([0-9]+\\):\\([0-9]+\\):?"))
 	 (let ((filename (buffer-substring (match-beginning 2) (match-end 2)))
 	       (linenum (string-to-number (buffer-substring (match-beginning 3) (match-end 3))))
 	       (colnum (if (match-beginning 4) (string-to-number (buffer-substring (match-beginning 4) (match-end 4))) 1))
