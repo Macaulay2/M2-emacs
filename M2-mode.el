bd2ba4bb6c097480d40ae8545034e00711cedd54
Doug Torrance <dtorrance@piedmont.edu>
2026-01-12 23:43:35 -0500

    checkdoc fixes for M2-mode.el

--- M2-mode.el
+++ M2-mode.el
@@ -6,9 +6,11 @@
 
 ;;; Commentary:
 ;; This file used to contain most of M2-mode definitions, which have since moved
-;; to M2.el. This file remains for compatibility, or for new features.
+;; to M2.el.  This file remains for compatibility, or for new features.
 
 (require 'M2)
+
+;;; Code:
 
 (provide 'M2-mode)
 
